eb0ce13c2dad6bd265b0ff4a332e1ebdebea6b48
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-05 11:42:54 -0800

    Add argument parser to make it easier to work with sample data

--- 2020/skel/challenge.py
+++ 2020/skel/challenge.py
@@ -1,5 +1,11 @@
-def get_input():
-    fin = open('input.list', 'r')
+import argparse
+
+def get_input(test):
+    fname = 'input.list'
+    if test:
+        fname = 'testdata'
+        print('USING TESTING DATA')
+    fin = open(fname, 'r')
     lines = fin.read().splitlines()
     fin.close()
 
@@ -21,7 +27,10 @@ def part1(input):
 
 
 def main():
-    input = get_input()
+    parser = argparse.ArgumentParser(description='Advent of code 2020 solutions by Alastair')
+    parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
+    args = parser.parse_args()
+    input = get_input(args.test)
     #part1(input)
     #part2(input)
 
