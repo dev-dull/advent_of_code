fbe9bd0c9f7757a5a37ceb1a8db792ad7d706656
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-06 09:58:53 -0800

    added solution for part 2

--- 2020/day06/challenge.py
+++ 2020/day06/challenge.py
@@ -9,6 +9,19 @@ class TravelGroup(list):
     def group_yes_count(self):
         return len(set(reduce(lambda a,b: a+b, self)))
 
+    def everyone_yes_count(self):
+        yeses = set(reduce(lambda a,b: a+b, self))
+        yesct = 0
+        for y in yeses:
+            all_yes = []
+            for person in self:
+                all_yes.append(y in person)
+
+            if all(all_yes):
+                yesct += 1
+
+        return yesct
+
 
 def get_input(test):
     fname = 'input.list'
@@ -26,24 +39,26 @@ def get_input(test):
 
 
 def part2(input):
-    pass
+    print(sum([gd.everyone_yes_count() for gd in input]))
 
 
 def part1(input):
-    group_data = []
-    for gd in input:
-        group_data.append(TravelGroup(gd))
-
-    print(sum([gd.group_yes_count() for gd in group_data]))
+    print(sum([gd.group_yes_count() for gd in input]))
 
 
 def main():
     parser = argparse.ArgumentParser(description='Advent of code 2020 solutions by Alastair')
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
+
     input = get_input(args.test)
-    part1(input)
-    #part2(input)
+
+    group_data = []
+    for gd in input:
+        group_data.append(TravelGroup(gd))
+
+    #part1(group_data)
+    part2(group_data)
 
 
 if __name__ == '__main__':
