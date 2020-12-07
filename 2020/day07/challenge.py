d65d7d4a8207d6fe44ed00d6e389ba11c16b6034
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-07 10:32:05 -0800

    Solved part 1

--- 2020/day07/challenge.py
+++ 2020/day07/challenge.py
@@ -1,5 +1,31 @@
 import argparse
 
+class Bags(dict):
+    def __init__(self, input):
+        for bag in input:
+            self.add_bag(bag)
+
+    def add_bag(self, raw_bag):
+        this_bag_parts = raw_bag.split(' bags contain ')
+        this_bag_name = this_bag_parts[0]
+
+        self[this_bag_name] = {}  # If I were to do this over again, a bag would have pointers to the bags it can contain.
+        if not this_bag_parts[1].startswith('no '):
+            for raw_contents in this_bag_parts[1].split(', '):
+                content_parts = raw_contents.split()
+                self[this_bag_name][' '.join(content_parts[1:-1])] = int(content_parts[0])
+
+    def holds_gold(self, color_name):
+        if 'shiny gold' in self[color_name]:
+            return True
+        elif not self[color_name]:
+            return False
+
+        any_gold = []
+        for color,ct in self[color_name].items():
+            any_gold.append(self.holds_gold(color))
+        return any(any_gold)
+
 def get_input(test):
     fname = 'input.list'
     if test:
@@ -12,10 +38,7 @@ def get_input(test):
     while '' in lines:
         lines.remove('')
 
-    retval = []
-    for line in lines:
-        retval.append(list(line))
-    return retval
+    return lines
 
 
 def part2(input):
@@ -23,15 +46,25 @@ def part2(input):
 
 
 def part1(input):
-    pass
-
+    gold_ct = 0
+    for color,contents in input.items():
+        if input.holds_gold(color):
+            gold_ct += 1
+            #print(color)
+    return gold_ct
 
 def main():
     parser = argparse.ArgumentParser(description='Advent of code 2020 solutions by Alastair')
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    #part1(input)
+
+    bags = Bags(input)
+
+    #import json
+    #print(json.dumps(bags, indent=2))
+
+    print(part1(bags))
     #part2(input)
 
 
