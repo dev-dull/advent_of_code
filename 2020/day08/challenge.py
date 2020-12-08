68f4b4ffc18a8b5c64ea16c219c5fce8ebae488f
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-08 07:50:23 -0800

    Created and added data for day 08

---
+++ 2020/day08/challenge.py
@@ -0,0 +1,39 @@
+import argparse
+
+def get_input(test):
+    fname = 'input.list'
+    if test:
+        fname = 'testdata'
+        print('USING TESTING DATA')
+    fin = open(fname, 'r')
+    lines = fin.read().splitlines()
+    fin.close()
+
+    while '' in lines:
+        lines.remove('')
+
+    retval = []
+    for line in lines:
+        retval.append(list(line))
+    return retval
+
+
+def part2(input):
+    pass
+
+
+def part1(input):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent of code 2020 solutions by Alastair')
+    parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
+    args = parser.parse_args()
+    input = get_input(args.test)
+    #part1(input)
+    #part2(input)
+
+
+if __name__ == '__main__':
+    main()
