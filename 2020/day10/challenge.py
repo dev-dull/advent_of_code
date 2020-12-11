2b553057f6b15667dadd2ff99f76b944e70c9f91
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-11 09:07:13 -0800

    Added partial solution to day10 part 2

--- 2020/day10/challenge.py
+++ 2020/day10/challenge.py
@@ -1,4 +1,5 @@
 import argparse
+from copy import copy
 from collections import defaultdict
 
 
@@ -16,9 +17,25 @@ def get_input(test):
 
     return [int(n) for n in lines]
 
+def _part2(input):
+    ### NOT working yet. I know I'm on the right track, though.
+    ## I need to pin down the cause of doups
+    ## I need to include the very first combo (the one with everything)
+    for i in range(1, len(input)-1):
+        #if input[i-1] - input[i] == 1 and input[i] - input[i+1] == 1:
+        if input[i+1] - input[i-1] <= 3:
+            c = copy(input)
+            c.remove(input[i])
+            _part2(c)
+            print(input)
+
 
 def part2(input):
-    pass
+    input.append(max(input)+3)
+    input.append(0)
+    input.sort()
+
+    _part2(input)
 
 
 def part1(input):
@@ -41,8 +58,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    part1(input)
-    #part2(input)
+    #part1(input)
+    part2(input)
 
 
 if __name__ == '__main__':
