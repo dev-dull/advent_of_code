d482ae605e2c9a190a209133c484795812047fc0
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-12 14:13:44 -0800

    Solved part 1

--- 2020/day12/challenge.py
+++ 2020/day12/challenge.py
@@ -12,10 +12,48 @@ def get_input(test):
     while '' in lines:
         lines.remove('')
 
-    retval = []
-    for line in lines:
-        retval.append(list(line))
-    return retval
+    return lines
+
+class Navigate(dict):
+    def __init__(self):
+        self.EAST = 0
+        self.SOUTH = 90
+        self.WEST = 180
+        self.NORTH = 270
+
+        self.direction = self.EAST
+        self[self.EAST] = 0
+        self[self.SOUTH] = 0
+        self[self.WEST] = 0
+        self[self.NORTH] = 0
+
+    def navigate(self, input):
+        for line in input:
+            func = eval('self.'+line[0])
+            func(int(line[1:]))
+
+        return abs(self[self.EAST] - self[self.WEST]),abs(self[self.NORTH] - self[self.SOUTH])
+
+    def N(self, distance):
+        self[self.NORTH] += distance
+
+    def S(self, distance):
+        self[self.SOUTH] += distance
+
+    def E(self, distance):
+        self[self.EAST] += distance
+
+    def W(self, distance):
+        self[self.WEST] += distance
+
+    def L(self, degrees):
+        self.direction = (self.direction - degrees) % 360
+
+    def R(self, degrees):
+        self.direction = (self.direction + degrees) % 360
+
+    def F(self, distance):
+        self[self.direction] += distance
 
 
 def part2(input):
@@ -23,7 +61,10 @@ def part2(input):
 
 
 def part1(input):
-    pass
+    n=Navigate()
+    ew,ns = n.navigate(input)
+
+    print('%s + %s = %s' % (ew, ns, ew+ns))
 
 
 def main():
@@ -31,7 +72,7 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    #part1(input)
+    part1(input)
     #part2(input)
 
 
