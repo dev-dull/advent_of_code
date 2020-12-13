f4fad34c8a86343f008f8d1df07d82a670d46c35
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-13 10:42:28 -0800

    Solved part 1

--- 2020/day13/challenge.py
+++ 2020/day13/challenge.py
@@ -12,18 +12,22 @@ def get_input(test):
     while '' in lines:
         lines.remove('')
 
-    retval = []
-    for line in lines:
-        retval.append(list(line))
-    return retval
+    return int(lines[0]),[n if n=='x' else int(n) for n in lines[1].split(',')]
 
 
 def part2(input):
     pass
 
 
-def part1(input):
-    pass
+def part1(ts, sched):
+    sched = list(filter(lambda n: isinstance(n, int), sched))
+    time_index = 0
+    while True:
+        time_index += 1
+        busses_now = list(filter(lambda b: time_index%b==0, sched))
+        if busses_now and time_index >= ts:
+            print('%s * %s = %s' % (min(busses_now), time_index-ts, (time_index-ts) * min(busses_now)))
+            break
 
 
 def main():
@@ -31,7 +35,7 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    #part1(input)
+    part1(*input)
     #part2(input)
 
 
