1983525ebf1143bf2a1c925d7b1edff9422da882
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-12 17:02:55 -0800

    Solved part 2

--- 2020/day12/challenge.py
+++ 2020/day12/challenge.py
@@ -56,8 +56,58 @@ class Navigate(dict):
         self[self.direction] += distance
 
 
+class Navigate2(Navigate):
+    def __init__(self):
+        super().__init__()
+        self.wp_x = 10  # waypoint x and y
+        self.wp_y = 1
+
+        self.pos_x = 0
+        self.pos_y = 0
+
+    def navigate(self, input):
+        for line in input:
+            func = eval('self.'+line[0])
+            func(int(line[1:]))
+
+        return abs(self.pos_x),abs(self.pos_y)
+        #return abs(self[self.EAST] - self[self.WEST]),abs(self[self.NORTH] - self[self.SOUTH])
+
+    def N(self, distance):
+        self.wp_y += distance
+
+    def S(self, distance):
+        self.wp_y -= distance
+
+    def E(self, distance):
+        self.wp_x += distance
+
+    def W(self, distance):
+        self.wp_x -= distance
+
+    def L(self, degrees):
+        # swap x and y, then x*=-1
+        for _ in range(0, degrees, 90):
+            tmp = self.wp_x
+            self.wp_x = self.wp_y * -1
+            self.wp_y = tmp
+
+    def R(self, degrees):
+        # swap x and y, then y*=-1
+        for _ in range(0, degrees, 90):
+            tmp = self.wp_x * -1
+            self.wp_x = self.wp_y
+            self.wp_y = tmp
+
+    def F(self, distance):
+        self.pos_x = self.pos_x + (self.wp_x * distance)
+        self.pos_y = self.pos_y + (self.wp_y * distance)
+
 def part2(input):
-    pass
+    n=Navigate2()
+    ew,ns = n.navigate(input)
+
+    print('%s + %s = %s' % (ew, ns, ew+ns))
 
 
 def part1(input):
@@ -72,8 +122,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    part1(input)
-    #part2(input)
+    #part1(input)
+    part2(input)
 
 
 if __name__ == '__main__':
