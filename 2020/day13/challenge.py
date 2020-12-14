45a2632b587af943d4349adf6c42df23c5d2c416
Alastair Llewellyn Drong <alastair@nomad.deep13.lol>
2020-12-13 20:29:38 -0800

    Takes F O R E V E R to run, but solved part 2 the non-mathy way

--- 2020/day13/challenge.py
+++ 2020/day13/challenge.py
@@ -16,8 +16,31 @@ def get_input(test):
 
 
 def part2(input):
-    pass
+    # key == what t%value needs to be
+    d = {}
+    for k,v in enumerate(input):
+        if isinstance(v, int):
+            d[k]=v
 
+    # There's clearly an equation that'll solve this problem a lot faster than a loop
+    # ...but right now, I'd rather kick things off and watch a documentary while this runs
+    ts = 100000000000000  # They told me it'd be bigger than this
+
+    while True:
+        found_it = []
+        for remainder,bus in d.items():
+            found_it.append((ts+remainder)%bus==0)
+
+        if not ts%100000:
+            print(ts, end='\r')
+
+        if all(found_it):
+            print('\n'+str(ts))
+            break
+        elif ts > 999999999999999:  # Stop for for the sake of sanity, and my CPU.
+            print('fail.')
+            break
+        ts += 1
 
 def part1(ts, sched):
     sched = list(filter(lambda n: isinstance(n, int), sched))
@@ -35,8 +58,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    part1(*input)
-    #part2(input)
+    #part1(*input)
+    part2(input[1])
 
 
 if __name__ == '__main__':
