6ef82d6d9266c1a80b5ad1d8ea8e157636036fb0
Alastair Drong <alastair.drong@infogroup.com>
2020-12-15 11:55:06 -0800

    Solved part 1. Based on what part 2 wants, there's probably a formula that lets me calculate the answer.

--- 2020/day15/challenge.py
+++ 2020/day15/challenge.py
@@ -14,25 +14,50 @@ def get_input(test):
 
     retval = []
     for line in lines:
-        retval.append(list(line))
+        retval.append([int(n) for n in line.split(',')])
     return retval
 
 
 def part2(input):
-    pass
+    part1(input, 30000000)  # clearly there's some math thing that'll get me to the answer faster, but /me gestures at my job. I'll just let this run.
 
 
-def part1(input):
-    pass
+def elf_game(has_been_spoken):
+    ct = len(has_been_spoken)+1
+    last_spoken_at  = list(range(1,ct))  # The turn number when the number in the parallel array was last spoken
+    speak = has_been_spoken[-1]
+    next_up = 0
 
+    while True:
+        yield (speak,ct)
+        speak = next_up
+        if next_up in has_been_spoken:
+            i = has_been_spoken.index(next_up)
+            next_up = ct - last_spoken_at[i]
+            last_spoken_at[i] = ct
+        else:
+            has_been_spoken.append(speak)
+            last_spoken_at.append(ct)
+            next_up = 0
+
+        ct += 1
+
+def part1(input, end_at):
+    eg = elf_game(input)
+    ct = 0
+    while ct < end_at+1:
+        spoken,ct = next(eg)
+    print(spoken, ct)
 
 def main():
     parser = argparse.ArgumentParser(description='Advent of code 2020 solutions by Alastair')
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
+
     input = get_input(args.test)
-    #part1(input)
-    #part2(input)
+    for l in input:
+        #part1(l, 2020)
+        part2(input)
 
 
 if __name__ == '__main__':
