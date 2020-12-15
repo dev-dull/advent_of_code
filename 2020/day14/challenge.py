786bb3df85a655337c0328f008e5a6115a3dd757
Alastair Drong <alastair.drong@infogroup.com>
2020-12-14 18:09:50 -0800

    Solved part 2 (in a way that's probably harder than it needs to be)

--- 2020/day14/challenge.py
+++ 2020/day14/challenge.py
@@ -15,6 +15,50 @@ def get_input(test):
     return lines
 
 
+class MemDock2(dict):
+    def __init__(self):
+        self.mask = []
+
+    def set_mask(self, mask):
+        self.mask = list(mask)
+        self.mask.reverse()  # put least significant digit at index 0
+
+    # I am 1000% sure I've made this harder than it needs to be.
+    # Plus, doing this way has some pass-by-ref side-effects one has to be careful of
+    def _get_all_floater_indicies(self, end_num, index_combos=[], start_num=0, result=[]):
+        for n in range(start_num, end_num):
+            self._get_all_floater_indicies(end_num, index_combos+[n], start_num=n+1, result=result)
+            result.append(index_combos+[n])
+        return result
+
+    def __setitem__(self, key, val):
+        d = {}
+        float_positions = []
+        for i,op in enumerate(self.mask):
+            if op == '1':
+                key = key | pow(2, i)
+            if op == 'X':
+                float_positions.append(pow(2, i))
+
+        # Set all float positions to 0 so we know our start value for the bit position
+        zeroed_floater_key = key
+        for n in float_positions:
+            if zeroed_floater_key & n:
+                zeroed_floater_key -= n
+
+        result = []
+        d[zeroed_floater_key] = val
+        floater_index_combos = self._get_all_floater_indicies(len(float_positions), result=result)
+        for fic in floater_index_combos:
+            float_val = 0
+            for fi in fic:
+                float_val+=float_positions[fi]
+
+            d[zeroed_floater_key+float_val] = val
+
+        self.update(d)
+
+
 class MemDock(dict):
     def __init__(self):
         self.mask = []
@@ -33,7 +77,17 @@ class MemDock(dict):
 
 
 def part2(input):
-    pass
+    md = MemDock2()
+    for line in input:
+        if line.startswith('mask'):
+            md.set_mask(line.split()[-1])
+        else:
+            sl = line.split()
+            key = int(sl[0].split('[')[-1][0:-1])
+            val = int(sl[-1])
+            md[key] = val
+
+    print(sum(md.values()))
 
 
 def part1(input):
@@ -55,8 +109,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    part1(input)
-    #part2(input)
+    #part1(input)
+    part2(input)
 
 
 if __name__ == '__main__':
