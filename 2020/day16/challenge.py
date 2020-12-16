28b9046f88cdbc0d6adb7edbf7a407b1ffb25f9b
Alastair Drong <alastair.drong@infogroup.com>
2020-12-16 13:48:38 -0800

    Solved part 2

--- 2020/day16/challenge.py
+++ 2020/day16/challenge.py
@@ -53,8 +53,55 @@ class TicketValidator(dict):
                 invalid_numbers.append(number)
         return invalid_numbers
 
-def part2(input):
-    pass
+    def identify_fields(self, number):
+        possible_fields = []
+        for rule_name,valid_ranges in self.items():
+            is_valid = False
+            for valid_range in valid_ranges:
+                is_valid = number in range(*valid_range)
+                if is_valid:
+                    break
+            if is_valid:
+                possible_fields.append(rule_name)
+        return possible_fields
+
+def part2(rules, my_ticket, nearby_tickets):
+    my_ticket = [int(n) for n in my_ticket]
+    nearby_tickets.append(my_ticket)
+    validator = TicketValidator(rules)
+    valid_tickets = []
+    for nearby_ticket in nearby_tickets:
+        if not validator.validate(nearby_ticket):
+            valid_tickets.append(nearby_ticket)
+
+    possible_fields_map = {}
+    for i in range(0,len(valid_tickets[0])):
+        possible_fields_map[i] = list(validator.keys())
+
+    for numbers in valid_tickets:
+        for i,number in enumerate(numbers):
+            possible_fields = validator.identify_fields(number)
+            for possible_field in possible_fields_map[i]:
+                if possible_field not in possible_fields:
+                    possible_fields_map[i].remove(possible_field)
+
+    while list(filter(lambda n: len(n)>1, possible_fields_map.values())):
+        known_field_positions = {}
+        for position,field_name in possible_fields_map.items():
+            if len(field_name) == 1:
+                known_field_positions[position] = field_name
+
+        for known_position,known_field_name in known_field_positions.items():
+            for position,field_names in possible_fields_map.items():
+                if known_position != position:
+                    if known_field_name[0] in field_names:
+                        possible_fields_map[position].remove(known_field_name[0])
+
+    departure_multi = 1
+    for position,name in possible_fields_map.items():
+        if name[0].startswith('departure'):
+            departure_multi *= my_ticket[position]
+    print(departure_multi)
 
 
 def part1(rules, my_ticket, nearby_tickets):
@@ -70,8 +117,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     rules,my_ticket,nearby_tickets = get_input(args.test)
-    part1(rules, my_ticket, nearby_tickets)
-    #part2(input)
+    #part1(rules, my_ticket, nearby_tickets)
+    part2(rules, my_ticket, nearby_tickets)
 
 
 if __name__ == '__main__':
