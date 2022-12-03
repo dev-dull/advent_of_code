d398089ec75db702bd77731a3c15935563626bf0
alastair.drong <alastair.drong@workday.com>
2022-12-03 10:06:49 -0800

    Solved part 2

--- 2022/day03/challenge.py
+++ 2022/day03/challenge.py
@@ -18,13 +18,30 @@ def get_input(test):
 
 
 def part2(input):
-    pass
+    priority_map = get_priority_map()
+    teams = []
+    previous = 0
+    for team in range(3, len(input)+1, 3):
+        # rejoin the bag contents so I don't have to change/break part1()
+        bags = [''.join(bag) for bag in input[previous:team]]
+        teams.append(bags)
+        previous = team
+
+    total = 0
+    for team in teams:
+        for c in team[0]:
+            has_c = [c in t for t in team[1:]]
+            if all(has_c):
+                # print(c, priority_map[c])
+                total += priority_map[c]
+                break
+    print(total)
 
 
 def part1(input):
     priority_map = get_priority_map()
     total = 0
-    for c1,c2 in input:
+    for c1, c2 in input:
         for l in c1:
             if l in c2:
                 # print(f'{l} {priority_map[l]}')
@@ -43,8 +60,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    part1(input)
-    #part2(input)
+    # part1(input)
+    part2(input)
 
 
 if __name__ == '__main__':
