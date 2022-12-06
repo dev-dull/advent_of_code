c1a1009b68520092a11b17ebeae54ceaff953be3
Alastair Llewellyn Drong <alastair@vger.deep13.lol>
2022-12-05 18:22:30 -0800

    POC method that should be able to handle more than 9 stacks

--- 2022/day05/challenge.py
+++ 2022/day05/challenge.py
@@ -36,6 +36,36 @@ def part1(*input):
     supply_stack.print_top_of_stack()
 
 
+def poc_stack_parse(stack):
+    # A proof-of-concept stack parse that should be able to handle >9 stacks
+    # Only works if your IDE did not screw up input data by stripping trailing spaces
+    # ...or if you know how to back-fill the 2d array with spaces.
+
+    # import inside the method so we're not eating ram for POC work that will go unused
+    import re
+    import numpy
+
+    np_stack = numpy.array([re.findall(' [0-9]+ |\[[A-Z]\]|    ', line) for line in stack])
+
+    # I'm not too familiar with numpy, but it seems there's no rot270()  :-(
+    np_stack = numpy.rot90(np_stack)
+    np_stack = numpy.rot90(np_stack)
+    np_stack = numpy.rot90(np_stack)
+
+    h_stack = []
+    for row in np_stack:
+        if row[0][1].isnumeric():
+            # I should probably use another regex here, but
+            h_stack.append([re.sub('[ \[\]]', '', r) for r in row])
+
+            # ew.
+            while '' in h_stack[-1]:
+                h_stack[-1].remove('')
+
+    for hs in h_stack:
+        print(hs)
+
+
 class SupplyStack(object):
     def __init__(self, stack):
         stack.reverse()  # flip the stack vertically, so it's easier to loop over.
@@ -81,6 +111,7 @@ def main():
     input = get_input(args.test)
     # part1(*input)
     part2(*input)
+    # poc_stack_parse(input[0])  # Thanks, me. I hate it.
 
 
 if __name__ == '__main__':
