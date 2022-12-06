99afff9f4e70f3c5cc72f858de5c31c8fb8b6f6a
alastair.drong <alastair.drong@workday.com>
2022-12-06 10:21:22 -0800

    Solved part 2 with copypasta

--- 2022/day06/challenge.py
+++ 2022/day06/challenge.py
@@ -14,7 +14,15 @@ def get_input(test):
 
 
 def part2(input):
-    pass
+    for data_string in input:
+        frame = [0, 14]
+        leni = len(data_string)  # Hello, this is Lenny.
+
+        while frame[1] + 1 < leni:
+            if len(set(data_string[frame[0]:frame[1]])) == len(data_string[frame[0]:frame[1]]):
+                print(data_string[frame[0]:frame[1]], data_string[frame[1] - 1], frame[1])
+                break
+            frame = [n + 1 for n in frame]
 
 
 def part1(input):
@@ -34,8 +42,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     input = get_input(args.test)
-    part1(input)
-    #part2(input)
+    # part1(input)
+    part2(input)
 
 
 if __name__ == '__main__':
