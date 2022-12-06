133ad9aea67ba5b426a4cecdd46e0fb0afec8805
alastair.drong <alastair.drong@workday.com>
2022-12-06 11:06:50 -0800

    Code de-dupe

--- 2022/day06/challenge.py
+++ 2022/day06/challenge.py
@@ -14,20 +14,12 @@ def get_input(test):
 
 
 def part2(input):
+    part1(input, frame_size=14)
+
+
+def part1(input, frame_size=4):
     for data_string in input:
-        frame = [0, 14]
-        leni = len(data_string)  # Hello, this is Lenny.
-
-        while frame[1] + 1 < leni:
-            if len(set(data_string[frame[0]:frame[1]])) == len(data_string[frame[0]:frame[1]]):
-                print(data_string[frame[0]:frame[1]], data_string[frame[1] - 1], frame[1])
-                break
-            frame = [n + 1 for n in frame]
-
-
-def part1(input):
-    for data_string in input:
-        frame = [0, 4]
+        frame = [0, frame_size]
         leni = len(data_string)  # Hello, this is Lenny.
 
         while frame[1]+1 < leni:
