24567b6e4d9532107209ec40d93143f7e5b3c78c
alastair.drong <alastair.drong@workday.com>
2022-12-07 17:34:41 -0800

    Solved part two after solving for the worng things, many, many times.

--- 2022/day07/challenge.py
+++ 2022/day07/challenge.py
@@ -14,7 +14,15 @@ def get_input(test):
 
 
 def part2(data):
-    pass
+    elfs = ElegantLogicalFileSystem()
+    for command in data:
+        if command:  # Toss out blank lines from ingestion
+            elfs.run_cmd(command)
+
+    space_used = elfs.refresh_size_on_disk()
+    space_needed = 30000000 - (70000000 - space_used)
+    # print(space_used, space_needed)
+    elfs.dirs_gte(space_needed)
 
 
 def part1(data):
@@ -23,13 +31,9 @@ def part1(data):
         if command:  # Toss out blank lines from ingestion
             elfs.run_cmd(command)
 
-    # import json
-    # print(json.dumps(elfs.elfs))
     elfs.sum_dirs()
 
 
-# If I had just done this all in a regular 'ol dictionary, I would've made life a lot better for myself.
-# Also, if I had added things recursively, I could've kept a running FS size total as I added stuff.
 class ElegantLogicalFileSystem(object):
     # ELFS header node
     def __init__(self):
@@ -48,10 +52,24 @@ class ElegantLogicalFileSystem(object):
             files = command_data.splitlines()
             self.cwd.ls(files[1:])
 
+    def refresh_size_on_disk(self):
+        sumdirs = 0
+        for k, v in self.elfs.items():
+            sumdirs += v.refresh_size_on_disk()
+        sumdirs += sum([f[0] for f in self.elfs.files])  # It took me way, WAY too long to spot that I was missing this.
+        self.elfs.size_on_disk = sumdirs  # As noted elsewhere, I should've just tracked this from the start
+        return sumdirs
+
     def sum_dirs(self):
         diskuse = []
         for k, v in self.elfs.items():
             v.sum_dirs(k, diskuse)
+        print(sum(diskuse), diskuse)
+
+    def dirs_gte(self, space_needed):
+        diskuse = [70000000]  # pass-by-ref-lazy-mode: activate!
+        for k, v in self.elfs.items():
+            v.dirs_gte(diskuse, space_needed)
         print(sum(diskuse), diskuse)
 
 
@@ -73,10 +91,21 @@ class _ELFS(dict):
                     _, dirname = dir_item.split()
                     self[dirname] = _ELFS(self)
                 if dir_item[0].isnumeric():
-                    size, filename = dir_item.split()
-                    self.files.append((int(size), filename))
+                    file_size, filename = dir_item.split()
+                    file_size = int(file_size)
+                    self.files.append((file_size, filename))
+
+    def refresh_size_on_disk(self):
+        # Would've been better to track this as files are added, but this is easier to groc.
+        sumdirs = 0
+        for k, v in self.items():
+            sumdirs += v.refresh_size_on_disk()
+        sumdirs += sum([f[0] for f in self.files])
+        self.size_on_disk = sumdirs
+        return sumdirs
 
     def sum_dirs(self, parent_key, diskuse, max_size=100000):
+        # Bah. `max_size` should've been an optional param at the ElegantLogicalFileSystem.sum_dirs()
         dirsum = 0
         for k, v in self.items():
             dirsum += v.sum_dirs(k, diskuse)
@@ -87,14 +116,21 @@ class _ELFS(dict):
             diskuse.append(dirsum)
         return dirsum
 
+    def dirs_gte(self, diskuse, space_needed):
+        if self.size_on_disk >= space_needed and self.size_on_disk < diskuse[0]:
+            diskuse[0] = self.size_on_disk
+        for k, v in self.items():
+            v.dirs_gte(diskuse, space_needed)
+        return diskuse
+
 
 def main():
     parser = argparse.ArgumentParser(description='Advent of code 2022 solutions by Alastair')
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     data = get_input(args.test)
-    part1(data)
-    #part2(data)
+    # part1(data)
+    part2(data)
 
 
 if __name__ == '__main__':
