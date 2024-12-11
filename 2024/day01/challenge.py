196cc78c217bb6bf5b63597749ad994c5ffb36e3
alastair.drong <alastair.drong@workday.com>
2024-12-11 14:41:58 -0800

    day 1

---
+++ 2024/day01/challenge.py
@@ -0,0 +1,37 @@
+import argparse
+
+
+def get_input(test):
+    fname = 'input.list'
+    if test:
+        fname = 'testdata'
+        print('USING TESTING DATA')
+
+    with open(fname, 'r') as fin:
+        lines = fin.read().strip().splitlines()
+
+    rows = [row.split() for row in lines]
+    return [int(i[0]) for i in rows],[int(i[1]) for i in rows]
+
+
+def part2(l, r):
+    print(sum([l[i]*r.count(l[i]) for i in range(0,len(l))]))
+
+
+def part1(l, r):
+    l.sort()
+    r.sort()
+    print(sum([abs(l[i]-r[i]) for i in range(0, len(l))]))
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent of code 2023 solutions by Alastair')
+    parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
+    args = parser.parse_args()
+    data = get_input(args.test)
+    #part1(*data)
+    part2(*data)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2024/skel/challenge.py
@@ -0,0 +1,34 @@
+import argparse
+
+
+def get_input(test):
+    fname = 'input.list'
+    if test:
+        fname = 'testdata'
+        print('USING TESTING DATA')
+
+    with open(fname, 'r') as fin:
+        lines = fin.read().strip().splitlines()
+
+    return lines
+
+
+def part2(data):
+    pass
+
+
+def part1(data):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Advent of code 2023 solutions by Alastair')
+    parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
+    args = parser.parse_args()
+    data = get_input(args.test)
+    #part1(data)
+    #part2(data)
+
+
+if __name__ == '__main__':
+    main()
