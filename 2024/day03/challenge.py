230c35c4c61c1f45f96fe4d55fcbcc0a493fcdfd
alastair.drong <alastair.drong@workday.com>
2024-12-12 15:07:05 -0800

    solve part 2 in still a pretty naughty way

--- 2024/day03/challenge.py
+++ 2024/day03/challenge.py
@@ -14,18 +14,27 @@ def get_input(test):
     return lines
 
 
-def part2(data):
-    pass
-
-
 # here's a bad plan that'll work
 # like, seriously, never, EVER blindly eval() strings you don't trust
 def mul(a, b):
     return int(a) * int(b)
 
 
+def part2(data):
+    do_mult = True
+    calculations = []
+    mul_exp = re.compile('mul\([0-9]{1,},[0-9]{1,}\)|((do|don\'t)\(\))')
+    for me in mul_exp.finditer(data):
+        operation = me.group()
+        if operation.startswith('do'):
+            do_mult = (operation == 'do()')
+
+        if do_mult and operation.startswith('mul('):
+            calculations.append(operation)
+    print(sum([eval(c) for c in calculations]))
+
+
 def part1(data):
-    print(data)
     mul_exp = re.compile('mul\([0-9]{1,},[0-9]{1,}\)')
 
     # here's a bad plan that'll work
@@ -40,8 +49,8 @@ def main():
     parser.add_argument('-t', '--test', dest='test', action='store_true', default=False, help='Use the file testdata instead of input.list')
     args = parser.parse_args()
     data = get_input(args.test)
-    part1(data)
-    #part2(data)
+    # part1(data)
+    part2(data)
 
 
 if __name__ == '__main__':
